2dd9480fe5b138f755ca55e4bc0e10ac8ebf41e2
gitkevinvera <ceo@webrend.com>
2025-03-09 23:58:44 -0400

    deleted everything

--- webrend/app/lib/github.ts
+++
@@ -1,8 +0,0 @@
-import { Octokit } from '@octokit/rest';
-
-export const GITHUB_TOKEN = process.env.GITHUB_TOKEN || '';
-export const GITHUB_USERNAME = process.env.GITHUB_USERNAME || '';
-
-export const octokit = new Octokit({
-  auth: GITHUB_TOKEN
-});
--- webrend/app/lib/stripe.ts
+++
@@ -1,6 +0,0 @@
-import Stripe from 'stripe';
-
-// Initialize Stripe with your secret key
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2024-11-20.acacia'
-});
